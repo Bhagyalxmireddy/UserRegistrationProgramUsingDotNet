62f3cee470814584a89b2e80324d4cc2a05d3528
Bhagi <puppyreddy1997@gmail.com>
2020-11-28 15:17:10 +0530

    [Bhagyalaxmi]ADDED:Added Code and Testcases to validate the firstname

---
+++ UserRegistrationTest/UserTest.cs
@@ -0,0 +1,41 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UserRegistrationProgram;
+
+namespace UserRegistrationTest
+{
+    [TestClass]
+    public class UserTest
+    {
+        UserValidations validations = new UserValidations();
+        [TestMethod]
+        public void givenFirstName_WhenProper_ShouldReturnTrue()
+        {
+            bool result = validations.firstName_Validation("BhagyaLaxmi");
+            Assert.IsTrue(result);
+        }
+        [TestMethod]
+        public void givenFirstName_WhenHaveSplChar_ShouldReturnFalse()
+        {
+            bool result = validations.firstName_Validation("Bh@gi");
+            Assert.IsFalse(result);
+        }
+        [TestMethod]
+        public void givenFirstName_WhenHaveNum_ShouldReturnFalse()
+        {
+            bool result = validations.firstName_Validation("Bhagi111");
+            Assert.IsFalse(result);
+        }
+        [TestMethod]
+        public void givenFirstName_WhenHaveSplCharAndNum_ShouldReturnFalse()
+        {
+            bool result = validations.firstName_Validation("Bh@gi11");
+            Assert.IsFalse(result);
+        }
+        [TestMethod]
+        public void givenFirstName_WhenHaveNull_ShouldReturnFalse()
+        {
+            bool result = validations.firstName_Validation("");
+            Assert.IsFalse(result);
+        }
+    }
+}
